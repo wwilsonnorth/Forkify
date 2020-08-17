d1eb1af6150e39b0693754a36387b57a62c716aa
William Wilson-North <wwilsonnorth@gmail.com>
2020-08-17 18:09:54 +0100

    moved index.html file and rebuilt to update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: './src/index.html'
+            template: './index.html'
         })
     ],
     module: {
